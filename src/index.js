6bde3b88767221c4dd5a77f2a0f2544b07200c7e
Tracy Reith <treith@treith.com>
2019-02-27 20:24:37 -0800

    Fix malformed jquery that prevented build - still left with small incorrect jquery behavior

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ console.log('hey');
 
 
 $(document).ready(function(){
-  $("#show-map").click() {
-    $(".map").show();
-  }
+  $("#show-map").click(function(){
+    var map = document.getElementById("map");
+    $("#map").toggle();
+    if (map.style.visibility === "none" ) {
+      $("#show-map").text("Show Map!");
+    } else {
+      $("#show-map").text("Hide Map");
+    }
+  });
 });
