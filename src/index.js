56f064a832fddc72cd01fd3f2041b448bd56a5fe
Student <example@example.com>
2019-02-28 17:12:26 -0800

    Fix jquery to show-hide map, add content to pages, add styling and images, add jquery for dropdown menu in mobile

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import $ from 'jquery';
 import './sass/styles.scss';
+import './sass/events.scss';
 console.log('hey');
 
-
 $(document).ready(function(){
-  $("#show-map").click(function(){
+  $(".show-map").click(function(){
     var map = document.getElementById("map");
     $("#map").toggle();
-    if (map.style.visibility === "none" ) {
-      $("#show-map").text("Show Map!");
+    if (map.style.display === "none" ) {
+      $(".show-map").text("Show Map!");
+      console.log("hello I can't see the map");
     } else {
-      $("#show-map").text("Hide Map");
+      $(".show-map").text("Hide Map");
+      console.log("hello I can see the map");
     }
+  });
+
+  $(".show-map2").click(function(){
+    var map = document.getElementById("map2");
+    $("#map2").toggle();
+    if (map.style.display === "none" ) {
+      $(".show-map2").text("Show Map!");
+      console.log("hello I can't see the map");
+    } else {
+      $(".show-map2").text("Hide Map");
+      console.log("hello I can see the map");
+    }
+  });
+
+  $(".menu").click(function(){
+    $(".nav").toggle(); 
   });
 });
